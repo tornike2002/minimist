2753bd524bee89ea2cf7fbf6f130dcd7a28459f4
tornike2002 <toko.toka2000@gmail.com>
2025-03-09 17:08:22 +0400

    add printHelp function and erorr function

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 #!/usr/bin/env node
 
+import minimist from "minimist";
 
-console.log(process.argv.slice(2));
+const args = minimist(process.argv.slice(2), {
+  boolean: ["help"],
+  string: ["file"],
+});
+
+if (args.help) {
+  printHelp();
+} else {
+  error("Incorrect Usage", true);
+}
+
+// ***************************************
+
+function printHelp() {
+  console.log("index.js usage:");
+  console.log("");
+  console.log("  index.js --help");
+  console.log("  index.js --file={FILENAME}");
+  console.log("");
+  console.log("--help                 print this help");
+  console.log("--file={FILENAME}      process the file");
+}
+
+function error(msg, includeHelp = false) {
+  console.error(msg);
+  if (includeHelp) {
+    console.log("");
+    printHelp();
+  }
+}
